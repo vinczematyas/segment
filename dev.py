8c1cfe04efd0dac6d0636377d80581af8037de1f
vinczematyas <matyas1vincze@gmail.com>
2024-05-03 13:06:45 +0200

    segformer init

--- dev.py
+++ dev.py
@@ -4,23 +4,64 @@ import matplotlib.pyplot as plt
 import PIL.Image as Image
 from glob import glob
 
-img = np.asarray(Image.open('./data/train/image/17gw5j.JPG'))
-label = np.load('./data/train/label/17gw5j_gt.npy')
+# from notebook
+from dataclasses import dataclass
 
-print(f'Shape of image: {img.shape}')
-print(f'Shape of label: {label.shape}')
+@dataclass
+class TrainingConfig:
+    batch_size: int = 8
+    epochs: int = 1
+    learning_rate: float = 1e-4
+    lr_decay_rate: float = 0.9998
+    seed: int = 420
+    model_name: str = 'nvidia/mit-b0'
+    project_name: str = 'segment'  # for wandb
+    device: Optional[str] = field(default_factory=lambda: 'cuda' if torch.cuda.is_available() else 'cpu')
 
-fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-ax[0].imshow(img)
-ax[0].set_title('Liptinite Image')
-ax[0].axis('off')
-ax[1].imshow(label)
-ax[1].set_title('Liptinite Label')
-ax[1].axis('off')
-ax[2].imshow(img)
-ax[2].set_title('Liptinite Image with Label')
-ax[2].axis('off')
-ax[2].imshow(label, alpha=0.5)
-plt.show()
+    def asdict(self):
+        return vars(self)
 
-# Path: dev.py
+cfg = TrainingConfig()
+
+ds = None  # TODO: load dataset
+ds.shuffle(seed=cfg.seed)
+
+id2label = {
+    "0": "lipnite",
+    "1": "vitrinite",
+    "2": "inertinite"
+}
+label2id = {v: k for k, v in id2label.items()}
+
+tokenizer = SegformerImageProcessor.from_pretrained(cfg.model_name)
+model = AutoModelForImageSegmentation.from_pretrained(
+    cfg.model_name, num_labeles = 3, id2label=id2label, label2id=label2id
+).to(cfg.device)
+
+# Image Transformations
+img_transforms = A.Compose(
+    [
+        A.HorizontalFlip(p=0.5),
+        A.OneOf(
+            [
+                A.Downscale(p=0.1, scale_min=0.4, scale_max=0.6),
+                A.GaussNoise(p=0.2),
+            ],
+            p=0.1,
+        ),
+        A.OneOf(
+            [
+                A.RandomBrightnessContrast(p=0.2),
+                A.ColorJitter(p=0.2),
+                A.HueSaturationValue(p=0.2),
+            ],
+            p=0.1,
+        ),
+        A.OneOf([A.PixelDropout(p=0.2), A.RandomGravel(p=0.2)], p=0.15),
+    ]
+)
+
+train_ds = None  # TODO: transform dataset using set_trainsform
+train_dl = DataLoader(train_ds, batch_size=cfg.batch_size, shuffle=True)
+
+# TODO: continue w. training loop from https://github.com/mattmdjaga/segformer_b2_clothes/blob/main/train_book.ipynb
